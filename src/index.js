3ce6011cb28fadc75c6c1f3eb678b67efdf64fb2
Einar Barzilay <EinarB@d.co.il>
2018-11-19 18:10:30 +0200

    load images

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import Image from './test.jpg';
 
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   return element;
+
+  var myImg = new image ();
+  myImg.src = Image;
+  element.appendChild(myImg);
 }
 
   document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+           'file-loader'
+         ]
       }
     ]
   }
-}
+};
